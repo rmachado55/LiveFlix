40da2dc7010d1657c63a85ca61b17115d9b12358
Ricardo Machado <ricardo.machado7@hotmail.com>
2022-04-13 14:48:24 -0300

    Add files via upload

---
+++ js/conteudo.js
@@ -0,0 +1,4 @@
+var bandas = ['Queen', 'Michael Jackson', 'Elvis Presley', 'Jimi Hendrix', 'Red Hot Chili Peppers', 'Beatles']
+var locais = ['Rio de Janeiro', 'Wembley', 'Onolulu', 'Bethel', 'Slane', 'Sidney',]
+var anos = ['(1985)', '(1988)', '(1973)', '(1969)', '(2003)', '(1964)']
+var paises = ['🇧🇷', '🏴󠁧󠁢󠁥󠁮󠁧󠁿', '🇺🇸', '🇺🇸', '🇮🇪', '🇦🇺']
